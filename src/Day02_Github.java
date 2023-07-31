9245df4d192e8141cfdeceae31387ce5e4471ec5
ümit sari <umitsari1986@hotmail.com>
2023-07-31 20:35:14 +0300

    ikinci satır eklendi

--- src/Day02_Github.java
+++ src/Day02_Github.java
@@ -1,6 +1,7 @@
 public class Day02_Github {
     public static void main(String[] args) {
         System.out.println("Merhaba Github");
+        System.out.println("İkinci satır ekleme işlemi");
 
     }
 }
